d4bc61ea1b873a0129d58bba98015e3f6c22860c
Даша <Teddi21girl@gmail.com>
2023-01-08 15:57:41 +0300

    added Selenide test

---
+++ src/test/java/github/SelenideRepositoryTest.java
@@ -0,0 +1,25 @@
+package github;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideRepositoryTest {
+
+    @Test
+    void shouldFindSelenideAsFirstRepository(){
+        Configuration.browser="chrome";
+        // открыть страницу https://github.com
+        open("https://github.com");
+        // ввести в поле поиска selenide и нажать Enter
+        $("[data-test-selector='nav-search-input']").setValue("selenide").pressEnter();
+        // нажать на линк от первого результата поиска
+        $$("ul.repo-list li").first().$("a").click();
+        // проверить: в заголовке встречается selenide/selenide
+        $("[class='v-align-middle']").shouldHave(text("selenide/selenide"));
+    }
+}
